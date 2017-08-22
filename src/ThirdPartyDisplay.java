93a3bda0986ad80433e6027e070862392c3eb401
Rick Myers <slipdizzy@gmail.com>
2017-08-21 23:33:00 -0400

    Committing to local repo

---
+++ src/ThirdPartyDisplay.java
@@ -0,0 +1,16 @@
+
+public class ThirdPartyDisplay implements DisplayElement, Observer {
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update (float temperature, float humidity, float pressure) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/WeatherData.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class WeatherData implements Subject {
+	private ArrayList<Observer> observers;
+	private float temperature, humidity, pressure;
+
+	public WeatherData() {
+		observers = new ArrayList<Observer>();
+	}
+
+	@Override
+	public void registerObserver(Observer o) {
+		observers.add(o);
+
+	}
+
+	@Override
+	public void removeObserver(Observer o) {
+		if (observers.contains(o)) {
+			observers.remove(o);
+		}
+	}
+
+	@Override
+	public void notifyObservers() {
+		for (Observer observer : observers) {
+			observer.update(temperature, humidity, pressure);
+		}
+
+	}
+
+	public float getTemperature() {
+		return this.temperature;
+	}
+
+	public float getHumidity() {
+		return this.humidity;
+	}
+
+	public float getPressure() {
+		return this.pressure;
+	}
+
+	public void measurementsChanged() {
+		notifyObservers();
+	}
+	
+	public void setMeasurements(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		this.pressure = pressure;
+		measurementsChanged();
+	}
+
+}
